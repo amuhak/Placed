7e4be479aaed8a4c067b912850c6512c5fcc346d
Amulya Jain <69428365+amuhak@users.noreply.github.com>
2022-08-27 20:08:07 +0530

    Update a.py

--- a.py
+++ a.py
@@ -34,17 +34,19 @@ print(df['PlacedOrNot'].value_counts())
 print(df['PlacedOrNot'].value_counts())
 y_values = df['PlacedOrNot']
 y_values = to_categorical(y_values)
-print(y_values)
+#print(y_values)
+print("TRAINING")
 model.fit(x_values,y_values,epochs=100,shuffle=True, batch_size=1)
 #may take a long time
 
-Age=22
-Gender=1
-Stream=3
-Internships=0
-CGPA=2
-Hostel=0
-HistoryOfBacklogs=1
+Age=int(input("enter age (int value) "))
+Gender=int(input("enter gender (int value) ")) #0==male,1==female
+Stream=int(input("enter stream (int vaue) "))
+Internships=int(input("enter internships (int vaue) "))
+CGPA=int(input("enter stream(int vaue) "))
+Hostel=int(input("enter Hostel(int vaue) "))
+HistoryOfBacklogs=int(input("enter if HistoryOfBacklogs(int vaue) "))
+print("Age,Gender,Stream,Internships,CGPA,Hostel,HistoryOfBacklogs")
 print(Age,Gender,Stream,Internships,CGPA,Hostel,HistoryOfBacklogs)
 in1=[[Age,Gender,Stream,Internships,CGPA,Hostel,HistoryOfBacklogs]]
 model.predict(in1)
